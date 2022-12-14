693ab3e23b47ffb8da4a9dfd92b7276b5006e257
Aljwhra <aljwhra.n2@icloud.com>
2022-12-14 09:45:35 +0300

    Initial Commit

---
+++ Muhmal/MuhmalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuhmalApp.swift
+//  Muhmal
+//
+//  Created by Aljwhra Alnasser on 20/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MuhmalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
